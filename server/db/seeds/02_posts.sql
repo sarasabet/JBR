1e7320fbfedda024a9c783a9c50106f7453545d1
Rupi-Jain <rupi.jain@gmail.com>
2021-11-22 17:19:49 -0500

    Setup database

---
+++ server/db/seeds/02_posts.sql
@@ -0,0 +1,43 @@
+INSERT INTO
+posts(url, user_id)
+VALUES
+    (
+        'Hey! Come back here! You big-a monkey!',
+        1
+    ),
+    (
+        'Nice of the princess to invite us over a picnic, eh Luigi?',
+        1
+    ),
+    (
+        'I wanna be a great plumber like my brother Mario.',
+        2
+    ),
+    (
+        'Luigi, Luigi! Oh yeah! Oh yeah!',
+        2
+    ),
+    (
+        'Did you see Toad and Chao and the sand?',
+        3
+    ),
+    (
+        'Aw, brutal!',
+        3
+    ),
+    (
+        'Whoo-hoo! Yippee!',
+        4
+    ),
+    (
+        'Aww, sweet! I did it, yay!',
+        4
+    ),
+    (
+        'Banana slamma!!',
+        5
+    ),
+    (
+        'Help, Diddy! I ve fallen, and I can t get up! Can you pass me a banana, little buddy?',
+        5
+    );
